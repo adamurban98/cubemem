17d8d000593178f7dbbdd76855d6b53e2f62cd15
Adam Urban <adamurban98@gmail.com>
2021-03-06 01:37:06 +0100

    minor change

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     args = request.args
-    return '<pre>'+'<br>'.join(['Hello, World!'] + [f'{k:50}: {v}' for k,v in args.items()])
+    return '<pre>'+'<br>'.join(['Hello, Adam!'] + [f'{k:50}: {v}' for k,v in args.items()])
 
 
 if __name__ == "__main__":
