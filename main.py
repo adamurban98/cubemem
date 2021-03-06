2beb0a53a24db9447be0ddc25db893db1a835600
Adam Urban <adamurban98@gmail.com>
2021-03-07 00:54:07 +0100

    final changes for today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request, render_template, url_for, session, redirect
+from flask import request, render_template, url_for, session, redirect, jsonify
 from datetime import timedelta
 import random
 from cube import Cube, DEFAULT_CUBECODE
@@ -15,15 +15,14 @@ def hello_world():
 @app.route('/cube')
 def cube():
     cubecode = request.args.get('cubecode', DEFAULT_CUBECODE) 
-    cubecode_userinput = request.args.get('cubecode-userinput', None) 
+    cubecode_userinput = request.args.get('cubecode-userinput', None)
     
     if cubecode_userinput is not None:
         cubecode = ''.join([c for c in cubecode_userinput if c in 'wbogry'])
         return redirect(url_for('cube', cubecode=cubecode))
     else:
         cube = Cube(cubecode)
-        return render_template('cube.html', cubecode=cube.cubecode, cube_color=cube.colors, cube_label=cube.strickers)
-
+        return render_template('cube.html', cubecode=cube.cubecode, cube_color=cube.colors, cube_label={})
 
 
 @app.route('/move')
@@ -37,14 +36,29 @@ def move():
     print(cube.strickers)
     return render_template('cube.html', cube_color=cube.colors, cube_label=cube.strickers)
 
-@app.route('/page/<num>')
-def view_page(num):
-    num = int(num)
-    session['path'] = session.get('path', []) + [num]
-    path = session['path']
-    return render_template('page.html', num=num, url=url_for('view_page', num=num*2), path=path)
 
+@app.route('/_parse-cubecode-userinput')
+def _verify_userinput():
     
+    cubecode_userinput = request.args.get('userinput', None)
+
+    print(list(request.args.items()))
+    print(list(request.form.items()))
+    if cubecode_userinput is not None:
+        cubecode = ''.join([c for c in cubecode_userinput if c in 'wbogry'])
+
+        if len(cubecode) > 3*3*6:
+            return jsonify(valid=False,message='Input too long.')
+        elif len(cubecode) < 3*3*6:
+            return jsonify(valid=False,message='Input too short.')
+        else:
+            return jsonify(valid=True, cubecode=cubecode, message='Input OK.')
+
+    else:
+        return jsonify(valid=False,message='No userinput provided.')
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
