493a876d993d488daa86bc00820146123f7f6316
Adam Urban <adamurban98@gmail.com>
2021-04-06 15:17:33 +0200

    improve moves to human readable moves transformation

--- moves.py
+++ moves.py
@@ -1,34 +1,49 @@
-import typing
-
-# Standard move notation       - S
-# single letter Coded notation - C
-
 _moves_capital = 'ULFRBDMESXYZ'
 
 
-def _c_to_s(c):
-    if c in _moves_capital:
-        return c 
+def _match_multimoves(moves):
+    '''
+    If given an array of moves return the longest consecutive prefix of moves that belong to and the tail.
+    e.g.:
+        - [U, u, U, L, R] -> [U, u, U], [L, r]
+        - [] -> [], []
+        - [U, U, U] -> [U, U, U], []
+        - [U] -> [U], []
+    '''
+
+    if len(moves) == 0:
+        return [], []
     else:
-        return str.upper(c) + "'"
+        first_move = moves[0].upper()
+        for i in range(1, len(moves)+1):
+            if i >= len(moves) or moves[i].upper() != first_move:
+                return moves[:i], moves[i:]
 
 
-def _s_to_c(s):
-    if len(s) == 1:
-        return s
-    else:
-        return str.lower(s[0])
+def moves_to_human_readable(moves):
+    assert(type(moves) == list), 'Move is not a list of chars'
 
+    moves_processed = []
+    while moves:
+        multimoves, moves = _match_multimoves(moves)
 
-def list_processing_helper(s, f):
-    if type(s) == str:
-        s = [s]
-    return [f(x) for x in s]
+        move = multimoves[0].upper()
+        forward = len([move for move in multimoves if move.isupper()])
+        backwards = len([move for move in multimoves if move.islower()])
 
+        multiplier = forward - backwards
+        multiplier_sign = abs(multiplier) // multiplier if multiplier != 0 else 0
+        multiplier_abs = abs(multiplier) % 4
 
-def moves_to_human_readable(c):
-    return list_processing_helper(c, _c_to_s)
+        multiplier_final = multiplier_sign * {0: 0, 1: 1, 2: 2, 3: -1}[multiplier_abs]
 
+        if multiplier_final == 0:
+            moves_processed += []
+        elif multiplier_final == 1:
+            moves_processed += [move]
+        elif multiplier_final == 2 or multiplier_final == -2:
+            moves_processed += [move + '2']
+        elif multiplier_final == -1:
+            moves_processed += [move + '\'']
 
-def s_to_c(s):
-    return list_processing_helper(s, _s_to_c)
+    return moves_processed
---
+++ test_moves.py
@@ -0,0 +1,31 @@
+from moves import moves_to_human_readable, _match_multimoves
+
+
+def test_simple():
+    moves_capital = 'ULFRBDMESXYZ'
+
+    moves = list(moves_capital) + list(moves_capital.lower())
+
+    human_readable_moves = moves_to_human_readable(moves)
+    human_readable_moves_reference = (
+        [move for move in moves_capital]
+        + [move + '\'' for move in moves_capital]
+    )
+
+    assert human_readable_moves == human_readable_moves_reference
+
+
+def test_match_multimoves():
+    assert _match_multimoves(['U', 'u', 'U', 'L', 'r']) == (['U', 'u', 'U'], ['L', 'r'])
+    assert _match_multimoves([]) == ([], [])
+    assert _match_multimoves(['U', 'u', 'U']) == (['U', 'u', 'U'], [])
+    assert _match_multimoves(['U']) == (['U'], [])
+
+
+def test_double_and_triple_moves():
+    assert moves_to_human_readable(list('LUUR')) == ['L', 'U2', 'R']        # UU
+    assert moves_to_human_readable(list('LUuR')) == ['L', 'R']              # Uu
+    assert moves_to_human_readable(list('LUuUR')) == ['L', 'U', 'R']        # UuU
+    assert moves_to_human_readable(list('LUuuUR')) == ['L', 'R']            # UuuU
+    assert moves_to_human_readable(list('LuuUUuuR')) == ['L', 'U2', 'R']  # uuUUuu
+    assert moves_to_human_readable(list('LUUUR')) == ['L', 'U\'', 'R']        # UUU
