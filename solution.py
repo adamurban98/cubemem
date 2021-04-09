e5fbdf63b6f21aaaec1234b485e4a30df3aa2134
Adam Urban <adamurban98@gmail.com>
2021-04-09 22:56:25 +0200

    refactor solution

--- solution.py
+++ solution.py
@@ -1,9 +1,4 @@
-from cube import Cube, corner_stickers, corner_stickers, lt_sticker_to_color
-from flask import url_for
-from collections import OrderedDict, namedtuple
-
 class Solution:
-
 
     @staticmethod
     def solve(cube, corner_buffer='A', edge_buffer='b'):
@@ -17,56 +12,56 @@ class Solution:
 
         # corners
         while not cube.corners_solved:
-            buffer_sticker = cube.stickers[corner_buffer] 
+            buffer_sticker = cube.stickers[corner_buffer]
 
             if cube.stickers[corner_buffer] in 'AER':
                 candidates = sorted(list(set(cube.unsolved_corner_positions) - set('AER')))
-                candidates += [corner_buffer] # This is the default choice
-                buffer_sticker = candidates[0] 
+                candidates += [corner_buffer]  # This is the default choice
+                buffer_sticker = candidates[0]
                 solution.corner_dummy_move.append(True)
             else:
                 solution.corner_dummy_move.append(False)
-            
+
             solution.corner_steps.append(buffer_sticker)
             solution.corner_states.append(cube)
 
             if buffer_sticker == corner_buffer:
                 # This is the end, everything is already solved
                 break
-            
+
             setup = cube.setup_moves(buffer_sticker)
             swapped = setup.alg_y()
             cube = swapped.undo_setup_moves(buffer_sticker)
 
         solution.state_after_corners = cube
-        
+
         solution.parity = len(solution.corner_steps) % 2 == 1
         if solution.parity:
             cube = cube.alg_r()
 
         # edges
         while not cube.edges_solved:
-            buffer_sticker = cube.stickers[edge_buffer] 
+            buffer_sticker = cube.stickers[edge_buffer]
 
             if cube.stickers[edge_buffer] in 'bm':
                 candidates = sorted(list(set(cube.unsolved_edge_positions) - set('bm')))
-                candidates += [edge_buffer] # This is the default choice
-                buffer_sticker = candidates[0] 
+                candidates += [edge_buffer]  # This is the default choice
+                buffer_sticker = candidates[0]
                 solution.edge_dummy_move.append(True)
             else:
                 solution.edge_dummy_move.append(False)
-            
+
             solution.edge_steps.append(buffer_sticker)
             solution.edge_states.append(cube)
 
             if buffer_sticker == edge_buffer:
                 # This is the end, everything is already solved
                 break
-            
+
             setup = cube.setup_moves(buffer_sticker)
             swapped = setup.alg_t()
             cube = swapped.undo_setup_moves(buffer_sticker)
-        
+
 
         solution.finalcube = cube
 
@@ -87,11 +82,3 @@ class Solution:
     @property
     def edge_mnemonic_pair_ques(self):
         return self._mnemonics_pair_helper(self.edge_steps)
-    
-
-if __name__ == '__main__':
-    cube = Cube().moves('B')
-    print(cube.text)
-    Solution.solve(cube)
-
-    
